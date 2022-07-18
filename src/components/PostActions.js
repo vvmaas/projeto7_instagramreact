c269800808e696110e73636c01fe5f2878f0d25a
Victor Vermaas <vhvermaas@gmail.com>
2022-07-18 15:21:27 -0300

    mudança de cor botao like

--- src/components/PostActions.js
+++ src/components/PostActions.js
@@ -1,24 +1,21 @@
 import React from 'react'
 
 export default function PostActions(){
-  const [curtida, setCurtida] = React.useState('heart-outline')
-  const [color, setColor] = React.useState("black")
+  const [curtida, setCurtida] = React.useState("heart-outline")
 
   function likeTog(){
-    if (curtida === 'heart-outline'){
-      setCurtida('heart')
-      setColor("red")
+    if (curtida === "heart-outline"){
+      setCurtida("heart");
     }
-    if (curtida === 'heart'){
-      setCurtida('heart-outline')
-      setColor("black")
+    if (curtida === "heart"){
+      setCurtida("heart-outline");
     }
   }
 
     return (      
   <div className="acoes">
     <div>
-      <ion-icon className={color} name={curtida} onClick={likeTog}></ion-icon>
+      <ion-icon  name={curtida} onClick={likeTog}></ion-icon>
       <ion-icon name="chatbubble-outline"></ion-icon>
       <ion-icon name="paper-plane-outline"></ion-icon>
     </div>
